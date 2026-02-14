2c029c84698795c536452e2163603da5f04968ad
Emersonlk <felipe-p@live.com>
2026-02-13 22:41:42 -0300

    ajuste no entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 if [ -f "artisan" ]; then
     php artisan migrate --force --no-interaction 2>/dev/null || true
+    php artisan db:seed --force --no-interaction 2>/dev/null || true
 fi
 
 exec "$@"
